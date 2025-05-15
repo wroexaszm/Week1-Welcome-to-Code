67aaadac669bb365c094701cf90d7984ba00f63c
wroexaszm <wroexaszm@proton.me>
2025-05-15 20:18:20 +0700

    push part 2

---
+++ quiz/part2/soal3.js
@@ -0,0 +1,16 @@
+// Problem
+// Mirip seperti soal nomor 2, namun kali ini gunakan substring untuk mengambil potongan dari tiap kata!
+
+let word3 = 'wow JavaScript is so cool';
+let exampleFirstWord3 = word3.substring(0, 3);
+
+console.log('First Word: ' + exampleFirstWord3);
+
+let exampleSecondWord3 = word3.substring(4, 14);
+console.log('Second Word: ' + exampleSecondWord3);
+let exampleThirdWord3 = word3.substring(15, 17);
+console.log('Third Word: ' + exampleThirdWord3);
+let exampleFourthWord3 = word3.substring(18, 20);
+console.log('Fourth Word: ' + exampleFourthWord3);
+let exampleFifthWord3 = word3.substring(21, 25);
+console.log('Fifth Word: ' + exampleFifthWord3);
